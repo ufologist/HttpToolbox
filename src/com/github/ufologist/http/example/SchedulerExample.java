4498d677e0c5670b3df90a938522a545d50dd532
Sun <klsforever@gmail.com>
2014-12-11 17:59:39 +0800

    在子线程中停止Scheduler无法结束JVM, 只能通过System.exit来退出JVM

--- src/com/github/ufologist/http/example/SchedulerExample.java
+++ src/com/github/ufologist/http/example/SchedulerExample.java
@@ -1,37 +1,39 @@
 package com.github.ufologist.http.example;
 
-import java.util.Date;
-
 import it.sauronsoftware.cron4j.Scheduler;
+
+import java.util.Date;
 
 public class SchedulerExample {
     public static void main(String[] args) {
-	testScheduler();
+        testScheduler();
     }
 
     private static void testScheduler() {
-	// Creates a Scheduler instance.
-	Scheduler scheduler = new Scheduler();
+        // Creates a Scheduler instance.
+        Scheduler scheduler = new Scheduler();
 
-	// Schedule a task.
-	// * * * * *
-	// 分 时 日 月 星
-	// http://www.sauronsoftware.it/projects/cron4j/manual.php#p02
-	scheduler.schedule("4 11 * * *", new Runnable() {
-	    public void run() {
-		// XXX 没有办法在子线程中停止Scheduler
-		// 暂时找不到停止的办法, 反正一般定时任务也是没有停止期限的
-		System.out.println(new Date());
-		System.out.println("task is run");
-	    }
-	});
+        // Schedule a task.
+        // * * * * *
+        // 分 时 日 月 星
+        // http://www.sauronsoftware.it/projects/cron4j/manual.php#p02
+        scheduler.schedule("4 11 * * *", new Runnable() {
+            public void run() {
+                System.out.println(new Date());
+                System.out.println("task is run");
+                // XXX 在子线程中停止Scheduler无法结束JVM
+                // 只能通过System.exit来退出JVM
+                // scheduler.stop();
+                // System.exit(0);
+            }
+        });
 
-	// Starts the scheduler.
-	scheduler.start();
-	// XXX 只能在主线程中停止Scheduler
-	// scheduler.stop();
+        // Starts the scheduler.
+        scheduler.start();
+        // XXX 在主线程中停止Scheduler会结束JVM
+        // scheduler.stop();
 
-	System.out.println(new Date());
-	System.out.println("Scheduler is start");
+        System.out.println(new Date());
+        System.out.println("Scheduler is start");
     }
 }
